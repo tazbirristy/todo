07ee27884834a640519c022678e7a4c9dc88308c
tazbirristy <tazbirristyweb@gmail.com>
2022-12-29 18:16:36 +0600

    complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzy8aFFqsVl0W0HUY3L1Z8D_z60jhIibI",
+  authDomain: "todo-10030.firebaseapp.com",
+  projectId: "todo-10030",
+  storageBucket: "todo-10030.appspot.com",
+  messagingSenderId: "772358277421",
+  appId: "1:772358277421:web:d537677f34a1b9b798aba4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
